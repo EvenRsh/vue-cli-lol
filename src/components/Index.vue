f908137b4c908580491e348995cc04ac95c9d207
EvenRH <15813307192@139.com>
2017-03-20 21:05:18 +0800

    填充resetCSS

---
+++ src/components/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="index">
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: 'index'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
